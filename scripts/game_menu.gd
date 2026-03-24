6b3ad1e0f9fcb2e6592426e07dbc4b7bf81e53fb
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2026-03-24 23:22:56 +0200

    wip: game menu and settings

---
+++ scripts/game_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+const MODAL_CONTAINER = preload("uid://ijw6ubii34sy")
+
+@onready var settings_button: TextureButton = %SettingsButton
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
